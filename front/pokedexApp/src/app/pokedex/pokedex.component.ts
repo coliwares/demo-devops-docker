4857bf06b4e87a75fdb8ab2176a44e2b25602e9e
Cristian Olivares <111800410+coliwares@users.noreply.github.com>
2024-05-23 20:57:04 -0400

    Add files via upload

---
+++ front/pokedexApp/src/app/pokedex/pokedex.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-pokedex',
+  templateUrl: './pokedex.component.html',
+  styleUrls: ['./pokedex.component.css']
+})
+export class PokedexComponent implements OnInit {
+  pokemon: any;
+  pokemonId: number = 1;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  searchPokemon() {
+    crossOriginIsolated: false;
+    this.http.get(`http://localhost:8080/pokedex/getPokemon/${this.pokemonId}` )
+      .subscribe(data => {
+        this.pokemon = data;
+      });
+  }
+}
